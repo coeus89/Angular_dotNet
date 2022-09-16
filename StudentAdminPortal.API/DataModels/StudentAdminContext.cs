0f770175842c643b2244f9756704c574378fc30c
Jonathan Karamavros <jkaramavros@live.com>
2022-09-16 15:51:08 -0400

    periodic checking v0.1

---
+++ StudentAdminPortal.API/DataModels/StudentAdminContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentAdminPortal.API.DataModels
+{
+    public class StudentAdminContext : DbContext
+    {
+        public StudentAdminContext(DbContextOptions<StudentAdminContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }  // differs from tutorial (Student)
+        public DbSet<Gender> Genders { get; set; }  // differs from tutorial (Gender)
+        public DbSet<Address> Addresses { get; set; }  // differs from tutorial (Address)
+    }
+}
--- StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.DataModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// This is the database context for the student admin console
+builder.Services.AddDbContext<StudentAdminContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAdminPortalDb")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
